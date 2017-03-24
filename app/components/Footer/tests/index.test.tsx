7acb42cf90814db0308f969939587b54f4c9edd1
Park Min-Sik <mspark@humaxdigital.com>
2017-03-24 15:52:27 +0900

    add LoopBack example

---
+++ app/components/Footer/tests/index.test.tsx
@@ -0,0 +1,11 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+
+import Footer from "../index";
+
+describe("<Footer />", () => {
+  it("should render the Footer", () => {
+    // const renderedComponent = shallow(<Footer />);
+  });
+});
+
--- server/models/person.js
+++ server/models/person.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(Person) {
-
+  Person.find = (id, cb) => {
+    process.nextTick(() => {
+      cb({
+        'firstname': 'ms',
+      });
+    });
+  };
 };
